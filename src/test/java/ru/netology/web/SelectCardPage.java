cc9f948fb4ecfb8df1cd4a0b4284aeca4c0da16d
Kharitonov Konstantin <karlyuhenson@gmail.com>
2020-04-22 00:15:36 +0300

    change SelenideElement twoCardTransferButton

--- src/test/java/ru/netology/web/SelectCardPage.java
+++ src/test/java/ru/netology/web/SelectCardPage.java
@@ -5,7 +5,7 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class SelectCardPage {
     private static SelenideElement oneCardTransferButton = $("[data-test-id='action-deposit']");
-    private static SelenideElement twoCardTransferButton = $("#root > div > ul > li:nth-child(2) > div > button");
+    private static SelenideElement twoCardTransferButton = $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d'] button");
     private static SelenideElement oneCardString = $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0']");
     private static SelenideElement twoCardString = $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d']");
 
